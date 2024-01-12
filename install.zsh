9132a391aeee35c7aab0cc170d60259a382e91ab
Dante Criscio <dantecriscio@gmail.com>
2024-01-12 12:57:24 -0500

    Made install.zsh only install omz if it doesn't exist yet

--- install.zsh
+++ install.zsh
@@ -14,4 +14,8 @@ brew install iterm2
 brew install --cask amethyst
 
 # Install oh-my-zsh but does not update any .zshrc config file or anything
-sh omz-installer.sh --unattended --keep-zshrc
+if ! [ -d "$HOME/.oh-my-zsh/" ]; then
+	sh omz-installer.sh --unattended --keep-zshrc
+else
+	echo "Oh-my-zsh already installed at $HOME/.oh-my-zsh/"
+fi
