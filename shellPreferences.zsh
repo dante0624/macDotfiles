fd5b95b1530f82e6d4737cc1aa347e43568c5847
Dante Criscio <dantcri@amazon.com>
2024-03-04 01:44:11 -0500

    Change omz color preferences

--- shellPreferences.zsh
+++ shellPreferences.zsh
@@ -65,7 +65,7 @@ function my_current_branch() {
 
 function ssh_connection() {
   if [[ -n $SSH_CONNECTION ]]; then
-    echo "%{$fg_bold[red]%}(ssh) "
+    echo "%{$FG[161]%}(ssh) "
   fi
 }
 
@@ -76,15 +76,14 @@ function _toolbox_prompt_info() {
 }
 
 local ret_status="%(?:%{$fg_bold[green]%}:%{$fg_bold[red]%})%?%{$reset_color%}"
-PROMPT=$'\n$(_toolbox_prompt_info)$(ssh_connection)%{$fg_bold[blue]%}%n@%m%{$reset_color%}$(my_git_prompt) : %~\n[${ret_status}] %# '
+PROMPT=$'\n$(_toolbox_prompt_info)$(ssh_connection)%{$FG[033]%}%n@%m%{$reset_color%}$(my_git_prompt) : %~\n[${ret_status}] %# '
 
 ZSH_THEME_PROMPT_RETURNCODE_PREFIX="%{$fg_bold[red]%}"
-ZSH_THEME_GIT_PROMPT_PREFIX=" $fg[white]‹ %{$fg_bold[yellow]%}"
-ZSH_THEME_GIT_PROMPT_AHEAD="%{$fg_bold[magenta]%}↑"
+ZSH_THEME_GIT_PROMPT_PREFIX=" $fg[white]‹ %{$FG[129]%}"
+ZSH_THEME_GIT_PROMPT_AHEAD="%{$fg_bold[green]%}↑"
 ZSH_THEME_GIT_PROMPT_BEHIND="%{$fg_bold[green]%}↓"
 ZSH_THEME_GIT_PROMPT_STAGED="%{$fg_bold[green]%}●"
 ZSH_THEME_GIT_PROMPT_UNSTAGED="%{$fg_bold[red]%}●"
 ZSH_THEME_GIT_PROMPT_UNTRACKED="%{$fg_bold[white]%}●"
 ZSH_THEME_GIT_PROMPT_UNMERGED="%{$fg_bold[red]%}✕"
 ZSH_THEME_GIT_PROMPT_SUFFIX=" $fg_bold[white]›%{$reset_color%}"
-
